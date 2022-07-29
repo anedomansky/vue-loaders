1d6620e28d89e2f18506e6bfeafe728fb3a4a080
anedomansky <nedomanskya@outlook.com>
2022-07-29 19:55:01 +0200

    - Plugin setup - rollup config setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,19 @@
-export default []
+import vue from "rollup-plugin-vue";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
+export default [
+  {
+    input: "src/index.js",
+    output: [
+      {
+        format: "esm",
+        file: "dist/library.mjs",
+      },
+      {
+        format: "cjs",
+        file: "dist/library.js",
+      },
+    ],
+    plugins: [vue(), peerDepsExternal()],
+  },
+];
---
+++ src/components.js
@@ -0,0 +1,21 @@
+import Box from "./Box.vue";
+import CirclingSquares from "./CirclingSquares.vue";
+import FlippingHexagons from "./FlippingHexagons.vue";
+import JumpingDots from "./JumpingDots.vue";
+import Overlay from "./Overlay.vue";
+import PulsatingDot from "./PulsatingDot.vue";
+import SpinningCircle from "./SpinningCircle.vue";
+import TravellingBar from "./TravellingBar.vue";
+import WanderingSquares from "./WanderingSquares.vue";
+
+export default {
+  Box,
+  CirclingSquares,
+  FlippingHexagons,
+  JumpingDots,
+  Overlay,
+  PulsatingDot,
+  SpinningCircle,
+  TravellingBar,
+  WanderingSquares,
+};
