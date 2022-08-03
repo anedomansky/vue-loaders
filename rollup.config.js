3dd1540ca78f040f2b803fae430022b663d48d36
anedomansky <nedomanskya@outlook.com>
2022-08-03 20:19:30 +0200

    - Added rollup plugin + config needed for CSS

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,21 @@
-import vue from "rollup-plugin-vue";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import vue from 'rollup-plugin-vue';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import css from 'rollup-plugin-css-only';
 
 export default [
   {
-    input: "src/index.js",
+    input: 'src/index.js',
     output: [
       {
-        format: "esm",
-        file: "dist/library.mjs",
+        format: 'esm',
+        file: 'dist/library.mjs',
       },
       {
-        format: "cjs",
-        file: "dist/library.js",
+        format: 'cjs',
+        file: 'dist/library.js',
       },
     ],
-    plugins: [vue(), peerDepsExternal()],
+    // css: false -> transforms <style>-blocks into imports so the css plugin can extract the styles
+    plugins: [css(), vue({ css: false }), peerDepsExternal()],
   },
 ];
