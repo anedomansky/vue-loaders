b44ee8491273c5674497dcbc39712f9d23f329c1
anedomansky <nedomanskya@outlook.com>
2022-09-05 19:53:49 +0200

    Worked on the FlippingHexagons.

--- src/components/FlippingHexagons.vue
+++ src/components/FlippingHexagons.vue
@@ -85,5 +85,66 @@
   }
 
   .flipping-hexagon {
+    --hexagon: 1vmin;
+    /* 1:square root of 3 */
+    --hexagon-border: calc(var(--hexagon) * 1.73205081);
+    --hexagon-width: calc(var(--hexagon-border) * 2);
+    --hexagon-height: calc(var(--hexagon) * 2);
+    --hexagon-color: #485665;
+
+    margin-top: var(--hexagon);
+    height: var(--hexagon-height);
+    width: var(--hexagon-width);
+    position: relative;
+    background-color: var(--hexagon-color);
+    animation: slide 2s linear infinite;
+  }
+
+  .flipping-hexagon:nth-of-type(2) {
+    animation-delay: 0.25s;
+  }
+
+  .flipping-hexagon:nth-of-type(3) {
+    animation-delay: 0.5s;
+  }
+
+  .flipping-hexagon::before,
+  .flipping-hexagon::after {
+    width: 0;
+    height: 0;
+    border-left: var(--hexagon-border) solid transparent;
+    border-right: var(--hexagon-border) solid transparent;
+    position: absolute;
+  }
+
+  .flipping-hexagon::before {
+    border-bottom: var(--hexagon) solid var(--hexagon-color);
+    top: calc(var(--hexagon) * -1);
+  }
+
+  .flipping-hexagon::after {
+    content: '';
+    border-top: var(--hexagon) solid var(--hexagon-color);
+    bottom: calc(var(--hexagon) * -1);
+  }
+
+  @keyframes slide {
+    0%,
+    10% {
+      opacity: 1;
+      transform: perspective(140px) rotateX(180deg);
+    }
+
+    25%,
+    75% {
+      opacity: 0.6;
+      transform: perspective(140px) rotateX(0deg);
+    }
+
+    90%,
+    100% {
+      opacity: 1;
+      transform: perspective(140px) rotateY(-180deg);
+    }
   }
 </style>
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { default as CirclingSquares } from './components/CirclingSquares.vue';
+export { default as FlippingHexagons } from './components/FlippingHexagons.vue';
